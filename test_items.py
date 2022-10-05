15f9d5b5b45c15a58b56479cfd3eb8f6fb6eca1a
EvgenijMashkovcev <teken25424@mail.ru>
2022-10-05 16:44:55 +0300

    Time delay increased to 30 seconds

--- test_items.py
+++ test_items.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 def test_button_add_to_basket_exist_on_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    time.sleep(10)
+    time.sleep(30)
     button_on_page = browser.find_element(By.XPATH, "//button[@class='btn btn-lg btn-primary btn-add-to-basket']")
     assert button_on_page, "The specified button was not found"
 
